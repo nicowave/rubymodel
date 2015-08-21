c2b4bae05441ff624913a53827134df35ee25a35
nicowave <geoidlife@gmail.com>
2015-08-21 13:39:00 -0700

    Updated some Ruby tutorial workpace files to improve commenting, get rid of unnecessary whitespace and carriage returns

--- controll.rb
+++ controll.rb
@@ -1,27 +1,21 @@
-#ruby course on treehouse.com
-#nicolas roldos
-#6.21.2015
-#
+# ruby course on treehouse.com
+# nicolas roldos
+# 6.21.2015
+# controll flows
 
 car1speed = 500
 car2speed = 300
 car2speed += 300
 
 if car1speed > car2speed
-	puts "car1, at #{car1speed} mph,\nis faster than car2, at #{car2speed} mph. \n\n"
+  puts "car1, at #{car1speed} mph,\nis faster than car2, at #{car2speed} mph. \n\n"
 end
-
-
-
-
 
 if car2speed > car1speed
-	puts "car2, at #{car2speed} mph,\nis faster than car1, at #{car1speed} mph. \n\n"
+  puts "car2, at #{car2speed} mph,\nis faster than car1, at #{car1speed} mph. \n\n"
 end
 
-
-
-   `car_speed = 65
+car_speed = 65
 speed_limit = 60
 
 if car_speed > speed_limit
