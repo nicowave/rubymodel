c2a392d97d5a16572deefc91515bedc36e720a5f
nicowave <geoidlife@gmail.com>
2015-09-02 18:48:21 -0700

    adding changes to nameclass.rb based on teamtreehouse.com tutorial on Attribute Writers and Accessors

--- nameclass.rb
+++ nameclass.rb
@@ -30,7 +30,6 @@ end
 # 'instantiate' 'Name' class with 'name' variable
 #   print out 'name' properties to console using 'name''s methods 
 name = Name.new
-
 puts name.title
 puts name.firstname
 puts name.midname
@@ -68,9 +67,10 @@ end
 
 # Below the 'Name' class, 'instantiate' a new 'Name' instance set to the variable 'name'
 #  with any 'title' you choose.
-name = NameName.new("mr.")
+	# => name = NameName.new("mr.")
+
 # Call the title method on the name instance.
-puts name.title
+	# => puts name.title
 
 # Above code passed the teamtreehouse.com code challenge and 
 #  earned an additional 12 points for completing the 'Variables and Methods' challenge!
@@ -81,18 +81,42 @@ puts name.title
 #  but Ruby gives us a shortcut in the form of an attribute reader.
 
 class NameThree
-	attr_reader :title, :name, :firstname, :midname, :lastname
-	def initialize(title, name, firstname, midname, lastname)
+	# => attr_reader :title, :firstname, :midname, :lastname
+	# => attr_writer :title
+	attr_reader :firstname, :midname, :lastname
+	attr_accessor :title, :midname
+	attr_writer :firstname
+	def initialize(title, firstname, midname, lastname)
 		@title = title
-		@first_name = first_name
-    @midde_name = middle_name
-    @last_name = last_name 
+		@firstname = firstname
+    @midname = midname
+    @lastname = lastname 
 	end
+	# => def title(new_title)
+		# => @title = new_title
+	# => end
 end
+
 # teamtreehouse.com Code Challenge 3:
 #  code above passed and earned an additional 12 points for completing 
 #  'the Attribute Readers' challenge in Ruby Objects and Classes
 
+myname = NameThree.new("Mr.", "Nicolas", "", "Roldos")
+puts myname.title + "\n" + 
+	myname.firstname + " " + 
+	myname.midname + "\n" + 
+	myname.lastname
+
+myname.title = "Dr."
+puts myname.title
+
+myname.firstname = "Nico"
+myname.midname = "J. "
+puts myname.firstname + " " + myname.midname + " " + myname.lastname 
+
+# Ruby gives us shortcuts to write common pieces of code. 
+# The process of accessing and 'writing' attributes in a 'class' can be done using 
+#  `attr_writer` and `attr_accessor` to ease repeating code.
 
 
 
